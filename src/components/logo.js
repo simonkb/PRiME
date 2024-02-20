7e59101c60bd91a22ede57de692940198c6783b5
simonkb <simonkebede12@gmail.com>
2024-02-20 22:20:07 +0400

    Updated the logo position

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,8 +1,8 @@
 import styles from "./logo.module.css";
-
 const Logo = () => {
   return (
     <div className={styles.logo}>
+      
       <img className={`${styles.vectorIcon} ${styles.rotateIcon}`} alt="" src="/vector.svg" />
       <div className={styles.prime}>PRiME</div>
     </div>
