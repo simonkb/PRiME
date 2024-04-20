4cf597616545183db5f31a5b724c6150638f20cc
simonkb <simonkebede12@gmail.com>
2024-04-20 12:44:46 +0400

    minor changes

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,16 @@ import Head from "next/head";
 import "./global.css";
 import { useRouter } from "next/router";
 import { auth } from "../config/firebaseConfig";
+import dotenv from 'dotenv';
+dotenv.config();
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
+    console.log(process.env.API_KEY);
+    console.log(process.env.API_URL);
+
     const isAuthenticated = auth.currentUser;
     if (isAuthenticated) {
       router.push("/home");
